24970c4216769fa193cf5b337a741650058ade30
austincwl <78631431+austincwl@users.noreply.github.com>
2023-04-25 23:32:25 -0500

    extractValu() working

--- 8_Advanced_Array_Methods/js-array-reduce-exercise-starter/js-array-reduce/reduce.js
+++ 8_Advanced_Array_Methods/js-array-reduce-exercise-starter/js-array-reduce/reduce.js
@@ -6,7 +6,13 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    
+    return arr.reduce(function(accum, value){
+        accum.push(value[key]);
+        return accum;
+    },[]);
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
