7b545786ab63de13413b2aae14dfdb1efc7d2c1e
Austin Cawley <austincwl@gmail.com>
2023-06-02 21:06:24 -0500

    jquery

--- 13_jQuery/jquery-dom/app.js
+++ 13_jQuery/jquery-dom/app.js
@@ -3,8 +3,11 @@ $(function(){
     console.log("let's party with jQuery");
 
     //add image-center class to images in the article tag
-    $("article image").addClass("image-center");
+    $("article img").addClass("image-center");
 
+    //give all images inside an artical tag the class "image center"
     $("article p:last-child").remove();
+
+
 });
 
