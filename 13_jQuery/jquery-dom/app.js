e938077f7c664e82a1a9bbacb8ff4eba198e8f37
Austin Cawley <austincwl@gmail.com>
2023-06-04 12:31:39 -0500

    jquery

--- 13_jQuery/jquery-dom/app.js
+++ 13_jQuery/jquery-dom/app.js
@@ -8,9 +8,31 @@ $(function(){
     //give all images inside an artical tag the class "image center"
     $("article p:last-child").remove();
 
-    $("#title").css("fontSize", Math.random()*100+1)
+    //random title font size
+    $("#title").css("fontSize", Math.random()*100+1);
 
-    $("ol")
+    //add new list item
+    $("ol").append("<li>new last list item</li>");
 
+    //empty the aside and replace it with a paragraph
+    $("aside").empty().append("<p>sorry for that list.<p>");
+
+    //set background color with three number at bottom
+    $(".form-control").on("change", function(){
+        let red = $(".form-control").eq(0).val();
+        let blue = $(".form-control").eq(1).val();
+        let green = $(".form-control").eq(2).val();
+        $("body").css("background-color", `rgb(${red},${green},${blue}`)
+    });
+
+    //remove meme when clicked
+    $("img").on("click",function(){$("img").remove()});
+
+    /*better way to do above: (only 1 selection)
+    $("img").on('click', function (e) {
+      $(e.target).remove();
+    });
+    */
+    
 });
 
