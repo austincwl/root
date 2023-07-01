f661d1c95483dce96d35dd36ce477c69365f4615
Austin Cawley <austincwl@gmail.com>
2023-06-30 23:26:33 -0500

    min max keys

--- 17_Python_Fund/python-ds-practice/38_min_max_key_in_dictionary/min_max_key_in_dictionary.py
+++ 17_Python_Fund/python-ds-practice/38_min_max_key_in_dictionary/min_max_key_in_dictionary.py
@@ -9,3 +9,12 @@ def min_max_keys(d):
         >>> min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"})
         ('apple', 'cherry')
     """
+    minObj = min(d)
+    maxObj = max(d)
+    #print(minObj)
+    #print(maxObj)
+    return (minObj,maxObj)
+
+
+#print(min_max_keys({2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e'}))
+#print(min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"}))
