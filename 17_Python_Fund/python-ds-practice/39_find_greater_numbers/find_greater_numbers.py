3c89974bcb9ecc72ff2e440a5303b9ceaf204fff
Austin Cawley <austincwl@gmail.com>
2023-06-30 23:53:47 -0500

    find greater numbers

--- 17_Python_Fund/python-ds-practice/39_find_greater_numbers/find_greater_numbers.py
+++ 17_Python_Fund/python-ds-practice/39_find_greater_numbers/find_greater_numbers.py
@@ -19,3 +19,14 @@ def find_greater_numbers(nums):
         >>> find_greater_numbers([])
         0
     """
+    if(len(nums) < 1):
+        return 0
+
+    ret = 0
+    for i in range(0,len(nums)-1):
+        for j in range(i,len(nums)):
+            #print("nums["+str(i)+"]="+str(nums[i])+",nums["+str(j)+"]="+str(nums[j]))
+            if(nums[j]>nums[i]):
+                ret = ret + 1
+    return ret
+        
