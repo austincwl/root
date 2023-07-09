041d665796e59b0434b9b1ba4419607d2aa31ecb
Austin Cawley <austincwl@gmail.com>
2023-07-09 00:26:06 -0500

    working

--- 18_Flask/madlibs/app.py
+++ 18_Flask/madlibs/app.py
@@ -2,3 +2,11 @@ from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 app = Flask("__name__")
 app.config['SECRET_KEY'] = "secret"
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def home_page():
+    """Shows Home Page"""
+    return render_template('base.html')
+
+
