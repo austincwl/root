43f8273a5de593befac4fa9e0262f9be4267d86a
Austin Cawley <austincwl@gmail.com>
2023-07-09 13:27:39 -0500

    madlibs completed:

--- 18_Flask/madlibs/app.py
+++ 18_Flask/madlibs/app.py
@@ -1,12 +1,23 @@
 from flask import Flask, request, render_template
+from stories import story
 from flask_debugtoolbar import DebugToolbarExtension
-app = Flask("__name__")
+
+app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
+
 debug = DebugToolbarExtension(app)
+
 
 @app.route('/')
 def home_page():
     """Shows Home Page"""
-    return render_template('base.html')
+    prompts = story.prompts 
+    return render_template('base.html', prompts=prompts)
+    
+@app.route('/story')
+def generate():
+    """Shows completed sentance"""
+    text = story.generate(request.args)
+    return render_template('story.html', story=text)
 
 
